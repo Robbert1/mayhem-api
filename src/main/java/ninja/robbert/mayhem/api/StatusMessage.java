d073c5f7e7cdefe6d1fc1e1080c8e0137b46e6e5
Robbert <robbert1@gmail.com>
2021-05-13 16:16:20 +0200

    add competition status/results to statusmessage

--- src/main/java/ninja/robbert/mayhem/api/StatusMessage.java
+++ src/main/java/ninja/robbert/mayhem/api/StatusMessage.java
@@ -11,7 +11,13 @@ public class StatusMessage implements OutputMessage {
     private List<Hero> opponent;
     private FightStatus status;
     private FightResult result;
+    private CompetitionStatus competitionStatus;
+    private List<PlayerResult> competitionResult;
     private Date timestamp;
+
+    public enum CompetitionStatus {
+        idle, started, finished
+    }
 
     public enum FightStatus {
         idle, ready, fighting, overtime, finished
@@ -21,16 +27,62 @@ public class StatusMessage implements OutputMessage {
         win, loss
     }
 
+    public static class PlayerResult {
+        private String name;
+        private int wins;
+        private int losses;
+
+        PlayerResult() {
+            // for jackson
+        }
+
+        public PlayerResult(final String name, final int wins, final int losses) {
+            this.name = name;
+            this.wins = wins;
+            this.losses = losses;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(final String name) {
+            this.name = name;
+        }
+
+        public int getWins() {
+            return wins;
+        }
+
+        public void setWins(final int wins) {
+            this.wins = wins;
+        }
+
+        public int getLosses() {
+            return losses;
+        }
+
+        public void setLosses(final int losses) {
+            this.losses = losses;
+        }
+    }
+
     StatusMessage() {
         // for jackson
     }
 
-    public StatusMessage(List<Hero> you, List<Hero> opponent, FightStatus status, FightResult result) {
+    public StatusMessage(List<Hero> you, List<Hero> opponent, FightStatus status, FightResult result, CompetitionStatus competitionStatus) {
+        this(you, opponent, status, result, competitionStatus, null);
+    }
+
+    public StatusMessage(List<Hero> you, List<Hero> opponent, FightStatus status, FightResult result, CompetitionStatus competitionStatus, List<PlayerResult> competitionResult) {
         this.you = you;
         this.opponent = opponent;
         this.status = status;
         this.result = result;
         this.timestamp = new Date();
+        this.competitionStatus = competitionStatus;
+        this.competitionResult = competitionResult;
     }
 
     public List<Hero> getYou() {
